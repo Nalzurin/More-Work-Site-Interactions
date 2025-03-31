25b222be354464fc8216da0fd396c9961ffd2821
Nalzurin <alexnormandi@ukr.net>
2025-04-01 02:57:32 +0300

    Initial files

---
+++ Source/HarmonyStarter.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace QuestSource
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyStarter
+    {
+        static HarmonyStarter()
+        {
+            Harmony harmony = new Harmony("Nalzurin.QuestSource");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Source/PatchQuestGeneration.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using HarmonyLib;
+using System.Reflection.Emit;
+using System.Reflection;
+using RimWorld.QuestGen;
+
+namespace QuestSource
+{
+
+    [HarmonyPatch]
+    public static class PatchQuestGeneration
+    {
+
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(QuestGen), nameof(QuestGen.Generate));
+        }
+        private static void Postfix(Quest __result, QuestScriptDef root, Slate initialVars)
+        {
+            TaggedString desc = __result.description;
+            desc += $"\n\n<color=#808080>{root.modContentPack.Name}</color>";
+            __result.description = desc;
+        }
+
+    }
+}
